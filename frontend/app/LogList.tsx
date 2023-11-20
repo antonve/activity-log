0ec0007ef7b0e0ab4055537a23afa93870174830
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-11-20 20:04:30 +0900

    tweak layout of list

--- frontend/app/LogList.tsx
+++ frontend/app/LogList.tsx
@@ -15,19 +15,18 @@ export default function LogList() {
 
   return (
     <table className="w-full">
-      <thead>
-        <tr>
-          <td>Date</td>
-          <td>Category</td>
-          <td>Content</td>
-        </tr>
-      </thead>
       <tbody>
         {logs.data.logs.map(log => (
-          <tr key={log.id}>
-            <td>{log.done_at.toISOString().split('T')[0]}</td>
-            <td>{log.category}</td>
-            <td>{log.content}</td>
+          <tr key={log.id} className="border-b border-slate-100">
+            <td className="w-20 px-4 py-2 whitespace-nowrap">
+              {log.done_at.toISOString().split('T')[0]}
+            </td>
+            <td className="w-16">
+              <span className="rounded bg-stone-300 px-2 py-1">
+                {log.category}
+              </span>
+            </td>
+            <td className="px-4 py-2">{log.content}</td>
           </tr>
         ))}
       </tbody>
